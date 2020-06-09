eb69c0855376d5d507fae84f3656f92bc388a533
Tung <nttung91@gmail.com>
2020-06-09 15:47:40 +0700

    -Update code

--- ios/Pods/Target Support Files/Pods-CustomFontExample-CustomFontExampleTests/Pods-CustomFontExample-CustomFontExampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CustomFontExample_CustomFontExampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_CustomFontExample_CustomFontExampleTests
-@end
--- ios/Pods/Target Support Files/Pods-CustomFontExample-tvOSTests/Pods-CustomFontExample-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CustomFontExample_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_CustomFontExample_tvOSTests
-@end
